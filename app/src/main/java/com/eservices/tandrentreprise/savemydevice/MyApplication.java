d1bdca9b57d2fe8c52fc58fc6b3da5969df80a9e
RadHisoka <radhi.jlidi@gmail.com>
2017-01-29 14:52:48 +0100

    #9 Ajout fonctionnalité Sign in / Login / Disconnect

--- app/src/main/java/com/eservices/tandrentreprise/savemydevice/MyApplication.java
+++ app/src/main/java/com/eservices/tandrentreprise/savemydevice/MyApplication.java
@@ -24,12 +24,12 @@ public class MyApplication extends Application {
 
         isConnect = false;
 
-        demands.add(new Demande("Problème ordinateur", "Orinateur ne démarre plus",       "000-555-0001", "d.depaul@gmail.com"));
-        demands.add(new Demande("Problème PC","Il ne démarre plus",    "000-555-0002", "a.schlueter@gmail.com"));
-        demands.add(new Demande("Problème tablette",   "Elle s'éteinds toute seule",   "000-555-0003", "b.carruthers@gmail.com"));
-        demands.add(new Demande("Problème fichier word",  "Il s'ouvre pas",     "000-555-0004", "g.brashler@gmail.com"));
-        demands.add(new Demande("Problème internet",  "Impossible d'acceder à internet",      "000-555-0005", "a.dammann@gmail.com"));
-        demands.add(new Demande("Lenteur internet",  "Navigateur plein de pub",      "000-555-0006", "s.harpole@gmail.com"));
+        demands.add(new Demande("Problème ordinateur", "Orinateur ne démarre plus",       "000-555-0001", 1));
+        demands.add(new Demande("Problème PC","Il ne démarre plus",    "000-555-0002", 2));
+        demands.add(new Demande("Problème tablette",   "Elle s'éteinds toute seule",   "000-555-0003", 3));
+        demands.add(new Demande("Problème fichier word",  "Il s'ouvre pas",     "000-555-0004", 4));
+        demands.add(new Demande("Problème internet",  "Impossible d'acceder à internet",      "000-555-0005", 5));
+        demands.add(new Demande("Lenteur internet",  "Navigateur plein de pub",      "000-555-0006",6));
 
 
 /*        myDemands.add(demands.get(1));
